037cca2bcb0aeaa526eecba52941b2bab44f7f58
John Cramp <john>
2013-09-23 22:23:16 -0700

    lfr

--- code.lua
+++ code.lua
@@ -13,4 +13,17 @@ function SlashCmdList.JRI_RAIDINFO(msg, editbox)
     local name, worldBossID, reset = GetSavedWorldBossInfo(i)
     print(name)
   end
+
+  for i = 1, GetNumRFDungeons() do
+    local id, name, typeID, subtype, minLevel, maxLevel, level3, level4, level5, result09, result10, result11, name2, result13, raidSize, description, result16, result17, result18 = GetRFDungeonInfo(i)
+    local numEncounters, numCompleted = GetLFGDungeonNumEncounters(id);
+    -- https://github.com/tekkub/wow-ui-source/blob/233e69aa6e44523d3b439c16b91013362059328e/FrameXML/LFGFrame.lua#L1553
+    -- not sure if there's a good way to show the bosses I *haven't* looted
+    for j = 1, numEncounters do
+      local bossName, texture, isKilled = GetLFGDungeonEncounterInfo(id, j)
+      if isKilled then
+        print(name .. ": " .. bossName)
+      end
+    end
+  end
 end
