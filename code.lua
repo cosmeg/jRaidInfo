a728c28d15530debfecfde35d02cd48ad27a4e68
John Cramp <john>
2013-09-23 23:08:12 -0700

    show progress fraction rather than kills

--- code.lua
+++ code.lua
@@ -1,4 +1,5 @@
--- /raidinfo is no longer broken but I've grown used to the test version
+-- /raidinfo is no longer broken but I've grown used to the text version
+
 SLASH_JRI_RAIDINFO1 = '/ri'
 function SlashCmdList.JRI_RAIDINFO(msg, editbox)
   for i = 1, GetNumSavedInstances() do
@@ -14,26 +15,27 @@ function SlashCmdList.JRI_RAIDINFO(msg, editbox)
     print(name)
   end
 
-  for i = 1, GetNumFlexRaidDungeons() do
-    local id, name, typeID, subtype, minLevel, maxLevel = GetFlexRaidDungeonInfo(i)
-    local numEncounters, numCompleted = GetLFGDungeonNumEncounters(id);
-    -- not sure if there's a good way to show the bosses I *haven't* looted
-    for j = 1, numEncounters do
-      local bossName, texture, isKilled = GetLFGDungeonEncounterInfo(id, j)
-      if isKilled then
-        print("Flex " .. name .. ": " .. bossName)
-      end
-    end
+  local flex_ids = {
+    726,
+    728,
+    --729,
+    --730
+  }
+  for i, id in ipairs(flex_ids) do
+    local dungeonName, typeId, subtypeID, minLvl, maxLvl, recLvl, minRecLvl, maxRecLvl, expansionId, groupId, textureName, difficulty, maxPlayers, dungeonDesc, isHoliday, repAmount, forceHide = GetLFGDungeonInfo(id)
+    local numEncounters, numCompleted = GetLFGDungeonNumEncounters(id)
+    print ("Flex " .. dungeonName .. ": " .. numCompleted .. "/4" )
   end
 
-  for i = 1, GetNumRFDungeons() do
-    local id, name, typeID, subtypeID, minLevel, maxLevel, recLevel, minRecLevel, maxRecLevel, expansionLevel, groupID, textureFilename, difficulty, maxPlayers, description, isHoliday = GetRFDungeonInfo(i)
-    local numEncounters, numCompleted = GetLFGDungeonNumEncounters(id);
-    for j = 1, numEncounters do
-      local bossName, texture, isKilled = GetLFGDungeonEncounterInfo(id, j)
-      if isKilled then
-        print("LFR " .. name .. ": " .. bossName)
-      end
-    end
+  local lfr_ids = {
+    716,
+    717,
+    --724,
+    --725
+  }
+  for i, id in ipairs(lfr_ids) do
+    local dungeonName, typeId, subtypeID, minLvl, maxLvl, recLvl, minRecLvl, maxRecLvl, expansionId, groupId, textureName, difficulty, maxPlayers, dungeonDesc, isHoliday, repAmount, forceHide = GetLFGDungeonInfo(id)
+    local numEncounters, numCompleted = GetLFGDungeonNumEncounters(id)
+    print ("LFR " .. dungeonName .. ": " .. numCompleted .. "/4" )
   end
 end
