a31b88fb03e680eaaf1d81d5ce6541a5cccf801c
John Cramp <jbc.git@gmail.com>
2016-08-17 13:08:12 -0700

    Show dungeons too

--- code.lua
+++ code.lua
@@ -4,8 +4,8 @@ SLASH_JRI_RAIDINFO1 = '/ri'
 function SlashCmdList.JRI_RAIDINFO(msg, editbox)
   for i = 1, GetNumSavedInstances() do
     local instanceName, instanceID, instanceReset, instanceDifficulty, locked, extended, instanceIDMostSig, isRaid, maxPlayers, difficultyName, maxBosses, defeatedBosses = GetSavedInstanceInfo(i)
-    if locked and isRaid then
-      print(GetSavedInstanceChatLink(i) ..
+    if locked then
+      print(difficultyName .. " " .. GetSavedInstanceChatLink(i) ..
             " (" .. defeatedBosses .. "/" .. maxBosses .. ")")
     end
   end
