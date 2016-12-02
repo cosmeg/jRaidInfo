990efe0889bc9e25957f99381885b359b07518c2
John Cramp <jbc.git@gmail.com>
2016-12-02 12:17:29 -0800

    Factor out shortage code

--- code.lua
+++ code.lua
@@ -11,11 +11,31 @@ local function bonusText(role)
 end
 
 
+local function shortageBonusRoles(id)
+    local _, forTank, forHealer, forDamage, _ =
+      GetLFGRoleShortageRewards(id, LFG_ROLE_SHORTAGE_RARE)
+    local canBeTank, canBeHealer, canBeDamager = C_LFGList.GetAvailableRoles()
+
+    local bonuses = {}
+
+    if forTank and canBeTank then
+      table.insert(bonuses, bonusText("TANK"))
+    elseif forHealer and canBeHealer then
+      table.insert(bonuses, bonusText("HEALER"))
+    elseif forDamage and canBeDamager then
+      table.insert(bonuses, bonusText("DAMAGER"))
+    end
+
+    return table.concat(bonuses, " ")
+end
+
+
 SLASH_JRI_RAIDINFO1 = '/ri'
 function SlashCmdList.JRI_RAIDINFO(msg, editbox)
   -- It's likely this information won't be correct until the next call
   RequestRaidInfo()
 
+  -- Traditional instances - shown in /raidinfo
   for i = 1, GetNumSavedInstances() do
     local _, _, _, _, locked, _, _, _, _, difficultyName, maxBosses,
       defeatedBosses = GetSavedInstanceInfo(i)
@@ -25,37 +45,26 @@ function SlashCmdList.JRI_RAIDINFO(msg, editbox)
     end
   end
 
+  -- World bosses. Does not work for Legion
   for i = 1, GetNumSavedWorldBosses() do
     local name, worldBossID, reset = GetSavedWorldBossInfo(i)
     print(name)
   end
 
+  -- Raid Finder
   for i = 1, GetNumRFDungeons() do
     local dungeonInfo = { GetRFDungeonInfo(i) }
     local id = dungeonInfo[1]
     local name = dungeonInfo[2]
     local isAvailable, _, _ = IsLFGDungeonJoinable(id)
     if isAvailable then
-      local _, forTank, forHealer, forDamage, _ =
-        GetLFGRoleShortageRewards(id, LFG_ROLE_SHORTAGE_RARE)
-      local canBeTank, canBeHealer, canBeDamager =
-        C_LFGList.GetAvailableRoles()
-
-      local bonuses = {}
-      if forTank and canBeTank then
-        table.insert(bonuses, bonusText("TANK"))
-      elseif forHealer and canBeHealer then
-        table.insert(bonuses, bonusText("HEALER"))
-      elseif forDamage and canBeDamager then
-        table.insert(bonuses, bonusText("DAMAGER"))
-      end
-
       local numEncounters, numCompleted = GetLFGDungeonNumEncounters(id)
       local color = numCompleted < numEncounters and "|CFFFF0000" or ""
       local out = "LFR "..name..": "..color..numCompleted.."|r/"..numEncounters
 
-      if table.getn(bonuses) ~= 0 then
-        out = out.." ("..table.concat(bonuses, " ")..")"
+      local bonuses = shortageBonusRoles(id)
+      if string.len(bonuses) > 0 then
+        out = out.." ("..bonuses..")"
       end
 
       print(out)
